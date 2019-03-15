9b473aca64e174192626924d1dda80cb9f2da38c
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-03-15 19:17:06 +0545

    make controller separated out

--- routes/users.js
+++ routes/users.js
@@ -1,100 +1,21 @@
-const router = require('express').Router();
-const passport = require('passport');
-const User = require('../models/User');
-const bcrypt = require('bcryptjs');
-const { checkAuth, checkGuest } = require('../config/checkAuth')
+const Router = require('express').Router();
 
-router.get('/login', checkGuest, (req, res) => {
-    res.render('users/login')
-})
+const { checkAuth, checkGuest } = require('../config/checkAuth');
 
-router.get('/register', checkGuest, (req, res) => {
-    res.render('users/register')
-})
+const userController = require("../controllers/userController");
 
-router.post('/register', (req, res) => {
-    const errors = [];
-    if (!req.body.name) {
-        errors.push({ text: 'Empty name' })
-    }
-    if (!req.body.email) {
-        errors.push({ text: 'Empty email' })
-    }
-    if (!req.body.phone) {
-        errors.push({ text: 'Empty phone number' })
-    }
-    if (!req.body.address) {
-        errors.push({ text: 'Empty address' })
-    }
-    if (req.body.password.length < 4) {
-        errors.push({ text: 'Password too short' })
-    }
-    if (req.body.password !== req.body.password2) {
-        errors.push({ text: 'Passwords do not match' })
-    }
+Router.route('/login')
+      .get(checkGuest, userController.getLoginForm)
+      .post(userController.loginUser)
 
-    if (errors.length > 0) {
-        console.log(errors)
-        res.render('users/register', {
-            errors: errors,
-            name: req.body.name,
-            citizenshipno: req.body.citizenshipno,
-            password: req.body.password,
-            password2: req.body.password2,
-            email: req.body.email,
-            phone: req.body.phone,
-            email: req.body.email,
-            address: req.body.address
-        })
-    } else {
-        User.findOne({ email: req.body.email })
-            .then(user => {
-                if (user) {
-                    res.redirect('/users/register')
-                } else {
-                    const newuser = {
-                        name: req.body.name,
-                        password: req.body.password,
-                        email: req.body.email,
-                        phone: req.body.phone,
-                        address: req.body.address,
-                        dob: req.body.dob,
-                        gender: req.body.gender,
-                        citizenshipno: req.body.citizenshipno,
-                    
-                       
-                    }
-                    bcrypt.genSalt(10, (err, salt) => {
-                        bcrypt.hash(newuser.password, salt, (err, hash) => {
-                            if (err) throw err;
-                            newuser.password = hash
-                            new User(newuser).save()
-                                .then(user => {
-                                    res.redirect('/users/login')
-                                })
-                                .catch(err => console.log(err))
-                        });
-                    });
-                }
-            })
-    }
+Router.route('/register')
+      .get(checkGuest, userController.getRegisterForm)
+      .post(userController.registerUser)
 
-})
+Router.get('/logout', userController.logoutUser)
 
-router.post('/login', (req, res, next) => {
-    passport.authenticate('local',
-        {
-            failureRedirect: '/users/login',
-            successRedirect: '/'
-        })(req, res, next)
-})
-
-router.get('/logout', (req, res) => {
-    req.logout();
-    res.redirect('/')
-})
-
-router.get('/verify', checkAuth, (req, res) => {
+Router.get('/verify', checkAuth, (req, res) => {
     res.send('Verified')
 })
-module.exports = router;
+
+module.exports = Router;
