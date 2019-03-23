2bfe6a1dffb6f3e51fb20a8565b147ed2d366923
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-03-23 12:57:17 +0545

    fix quotes on edit page

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,21 @@
 const Router = require('express').Router();
 
+const request = require("request");
+
 const homeController = require('../controllers/homeController');
 const {checkAuth} = require('../config/checkAuth');
 
 Router.get('/', homeController.getHomePage)
 
 Router.get('/about', homeController.getAboutPage)
+
+Router.get('/sup', (req, res) =>{
+ request('https://jsonplaceholder.typicode.com/todos/1', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    var info = JSON.parse(body)
+    res.send(info);
+  }
+})
+})
 
 module.exports = Router;
