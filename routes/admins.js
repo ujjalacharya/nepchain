7cd2e099bc9e79857f5ce458f15df47f316c37c3
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-03-23 16:11:01 +0545

    add different navbar at admin

--- routes/admins.js
+++ routes/admins.js
@@ -3,6 +3,12 @@ const Router = require('express').Router();
 const {checkAuthAdmin } = require('../config/checkAuth');
 
 const adminController = require("../controllers/adminController");
+
+Router.all('/*',(req,res,next) => {
+    req.app.locals.layout ='admin';
+    next();
+});
+
 Router.get('/login',adminController.getLoginForm)
     .post("/login",adminController.loginAdmin)
 Router.get('/dashboard',adminController.dashboard)
