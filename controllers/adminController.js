764fd0ad96fa99f3186ffe5e2d30ff863174716a
AanandBhandari <aanandbhandari143@gmail.com>
2019-03-24 13:12:50 +0545

    verify unverify citizen

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,6 +1,8 @@
 const passport = require("passport");
 const Admin = require("../models/Admin");
+const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const Citizenship = require('../models/Citizenship');
 exports.getLoginForm = (req, res) => {
   res.render("admin/login", { layout: false });
 };
@@ -13,5 +15,26 @@ exports.loginAdmin = (req, res, next) => {
 };
 
 exports.dashboard = (req, res) => {
-  res.render("admin/dashboard");
+  // though it shld b req.admin bt this dsnt work i dont no know y!!
+  const adminWard = req.user.assignedWard;
+  Citizenship.find({ward : adminWard}).then(citizenships => {
+    if (citizenships) {
+      res.render("admin/dashboard",{citizenships});
+    }
+  }).catch(e => console.log(e))
+  
+};
+exports.verifiedRequset = (req,res) => {
+  Citizenship.findById(req.params.id).then(citizenship => {
+    console.log("citizenship");
+    
+      citizenship.isVerified = !citizenship.isVerified;
+      citizenship.save().then(verifiedCitizen => res.redirect("/admin/dashboard") );
+    
+    
+  }).catch(e => console.log(e))
+}
+exports.logoutAdmin = (req, res) => {
+  req.logout();
+  res.redirect("/admin/login");
 };
